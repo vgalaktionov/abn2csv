af8154e52dd64305e67312439aa61284bb605c9d
Vadim Galaktionov <v.galaktionov@enpicom.com>
2021-12-05 11:36:06 +0100

    add proper head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,16 @@ export default class Document extends NextDocument {
     render() {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <title>ABN2CSV</title>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                    <link rel="manifest" href="/site.webmanifest" />
+                    <meta name="description" content="Convert ABN AMRO MT940 and ICS PDF statements to CSV" />
+                    <meta name="keywords" content="ABN AMRO, transactions, export, ICS, account, credit card, csv" />
+                    <meta name="author" content="Vadim Galaktionov" />
+                </Head>
                 <body>
                     <ColorModeScript initialColorMode="system" />
                     <Main />
